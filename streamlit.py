a70a076a4bb71f90cbf11dbd0d86dbf1e4c73d8d
Dimacat-exe <141384815+Dimacat-exe@users.noreply.github.com>
2024-07-23 22:04:07 +0700

    Add back download code

--- streamlit.py
+++ streamlit.py
@@ -7,7 +7,21 @@ from ultralytics import YOLOv10
 import gdown
 import requests
 
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
 
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                
 def download_file_from_google_drive(id, destination):
     URL = "https://docs.google.com/uc?export=download"
     session = requests.Session()
