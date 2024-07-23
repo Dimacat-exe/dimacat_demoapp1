6e2c6d57335196bb0d822dc2bfe2503f9bd8210b
Dimacat-exe <141384815+Dimacat-exe@users.noreply.github.com>
2024-07-23 22:02:35 +0700

    Fix some problem as I misunderstanding the code

--- streamlit.py
+++ streamlit.py
@@ -8,21 +8,8 @@ import gdown
 import requests
 
 
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-    return None
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk: # filter out keep-alive new chunks
-                f.write(chunk)
-
 def download_file_from_google_drive(id, destination):
-    URL = "https://drive.usercontent.google.com/download?id=1ojdmsPdorikmdlxD0vA71gNJ0929aTXK&export=download"
+    URL = "https://docs.google.com/uc?export=download"
     session = requests.Session()
     response = session.get(URL, params = { 'id' : id }, stream = True)
     token = get_confirm_token(response)
@@ -31,7 +18,11 @@ def download_file_from_google_drive(id, destination):
         response = session.get(URL, params = params, stream = True)
     save_response_content(response, destination) 
 
-model = YOLOv10('/mount/src/dimacat_demoapp1/catdetect.pt')
+file_id = '1ojdmsPdorikmdlxD0vA71gNJ0929aTXK'
+destination = '/mount/src/dimacat_demoapp1/catdetect.pt'
+download_file_from_google_drive(file_id, destination)
+
+model = YOLOv10(destination)
 
 st.set_page_config(
     page_title='Find Cats',
