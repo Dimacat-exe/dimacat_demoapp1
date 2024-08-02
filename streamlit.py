4320e54966f90ca0618f688532d85780887ee53b
Dimacat-exe <141384815+Dimacat-exe@users.noreply.github.com>
2024-08-02 22:05:19 +0700

    Another fix to the process

--- streamlit.py
+++ streamlit.py
@@ -3,31 +3,27 @@ from PIL import Image
 import cv2
 import numpy as np
 from io import BytesIO
-from ultralytics import YOLOv10
+from ultralytics import YOLO
 import requests
+import tempfile
+import os
 
 URL = "https://github.com/Dimacat-exe/dimacat_demoapp1/releases/download/model2/best.pt"
 SAVE_AS = "best.pt"
 resp = requests.get(URL)
 with open(SAVE_AS, "wb") as f: 
     f.write(resp.content)
-
-model = YOLOv10('/mount/src/dimacat_demoapp1/best.pt')
-
+model = YOLO(SAVE_AS)
 st.set_page_config(
     page_title='Find Cats',
     initial_sidebar_state='expanded',
 )
-
 st.title('Find Cats')
-st.write('Upload your images and video here')
-
+st.write('Upload your images and videos here')
 uploaded_files = st.file_uploader(
     'Choose up to 50 image files:', type=['jpg', 'jpeg', 'png'], accept_multiple_files=True)
-
 uploaded_video = st.file_uploader(
     'Choose a video file:', type=['mp4', 'avi'])
-
 if uploaded_files:
     col1, col2 = st.columns(2)
     for i, uploaded_file in enumerate(uploaded_files):
@@ -43,12 +39,14 @@ if uploaded_files:
             st.image(img_out,
                      caption=f'Cats in image {i+1}',
                      use_column_width=True)
-
 if uploaded_video:
     st.write('Analyzing the video...')
     try:
         video_bytes = uploaded_video.read()
-        video = cv2.VideoCapture(BytesIO(video_bytes).read())
+        with tempfile.NamedTemporaryFile(delete=False) as tmp:
+            tmp.write(video_bytes)
+            tmp_path = tmp.name
+        video = cv2.VideoCapture(tmp_path)
         frame_count = 0
         fps = video.get(cv2.CAP_PROP_FPS)
         width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -56,7 +54,6 @@ if uploaded_video:
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         output_file = 'output_video.mp4'
         out_video = cv2.VideoWriter(output_file, fourcc, fps, (width, height))
-
         while True:
             ret, frame = video.read()
             if not ret:
@@ -64,13 +61,13 @@ if uploaded_video:
             with st.spinner(f'Detecting cats in frame {frame_count}...'):
                 results = model(frame, conf=0.15)
                 frame_out = results[0].plot()
-            out_video.write(frame_out)
-            frame_count += 1
-
+                frame_out = cv2.cvtColor(frame_out, cv2.COLOR_BGR2RGB)
+                out_video.write(frame_out)
+                frame_count += 1
         video.release()
         out_video.release()
-        
         st.write('Done!')
         st.video(output_file)
+        os.remove(tmp_path)
     except Exception as e:
         st.write(f"Error processing the video: {e}")
