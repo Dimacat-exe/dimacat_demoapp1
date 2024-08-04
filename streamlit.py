29c44b86c19737bc5d5a77ea6f2e09203b2ba901
Dimacat-exe <141384815+Dimacat-exe@users.noreply.github.com>
2024-08-04 17:34:48 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,12 +1,10 @@
 import streamlit as st
 from PIL import Image
-import cv2
 import numpy as np
 from io import BytesIO
 from ultralytics import YOLOv10
 import requests
 import supervision as sv
-import random
 
 # Function to download the model
 def download_model(url, save_as):
@@ -33,22 +31,23 @@ uploaded_files = st.file_uploader(
     'Choose up to 200 images:', type=['jpg', 'jpeg', 'png'], accept_multiple_files=True
 )
 
-# Detect cats in uploaded images
+# Detect and annotate cats in uploaded images
 if uploaded_files:
     col1, col2 = st.columns(2)
     for i, uploaded_file in enumerate(uploaded_files):
         img = Image.open(uploaded_file).convert("RGB")
+        img_np = np.array(img)
         with col1:
             st.image(img, caption=f'Image {i + 1}', use_column_width=True)
         with col2:
             with st.spinner(f'Detecting cats in image {i + 1}...'):
-                results = model(img, conf=0.15)[0]
+                results = model(img_np, conf=0.15)[0]
                 detections = sv.Detections.from_ultralytics(results)
                 bounding_box_annotator = sv.BoundingBoxAnnotator()
                 label_annotator = sv.LabelAnnotator()
                 img_out = bounding_box_annotator.annotate(
-                    scene=np.array(img), detections=detections)
+                    scene=img_np, detections=detections)
                 img_out = label_annotator.annotate(
                     scene=img_out, detections=detections)
-                img_out = Image.fromarray(cv2.cvtColor(img_out, cv2.COLOR_BGR2RGB))
+                img_out = Image.fromarray(img_out)
             st.image(img_out, caption=f'Cats in image {i + 1}', use_column_width=True)
